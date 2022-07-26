d025aafc8308359d3736d9325c0fa1180301d1c0
Matthias Buttgereit <matthias.buttgereit@outlook.com>
2022-07-26 18:03:24 +0200

    Colored sphere in different colors

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,8 +7,10 @@ use raytracing::{
 };
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, 1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
 
     let unit_direction = unit_vector(r.direction());
@@ -16,14 +18,18 @@ fn ray_color(r: Ray) -> Color {
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
-fn hit_sphere(center: Point3, radius: f64, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
     let oc = r.origin() - center;
     let a = dot(&r.direction(), &r.direction());
     let b = 2.0 * dot(&oc, &r.direction());
     let c = dot(&oc, &oc) - radius * radius;
     let discrim = b * b - 4.0 * a * c;
 
-    discrim > 0.0
+    if discrim < 0.0 {
+        -1.0
+    } else {
+        (-b - discrim.sqrt()) / (2.0 * a)
+    }
 }
 
 fn main() {
